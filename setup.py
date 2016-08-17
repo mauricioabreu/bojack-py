f30f5f350aca8d2bac38f83f6ace5fb65ccb2688
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-08-17 01:01:09 -0300

    feat: Add packaging files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='bojack',
+    version='0.0.1.dev0',
+    url='https://github.com/mauricioabreu/bojack-py',
+    license='MIT',
+    description='BoJack client written in Python',
+    author='Mauricio de Abreu Antunes',
+    author_email='mauricio.abreua@gmail.com',
+    packages=['bojack'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
